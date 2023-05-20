d2fd8f11ccecb8248586af8f8fc0c655217c20f1
SyrianSam <sami.k92@gmail.com>
2023-05-20 19:32:00 +0200

    refactor

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,126 +1,86 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
-
     public static int numEnemy = 0;
-
     public CharacterController2D controller;
     public Animator animator;
     public GameObject enemy;
-    //public static bool jumpVar = true;
-    bool jump = false;
-
-
-
-
     public float runSpeed = 40f;
-
-    float horizontalMove = 0f;
-    int levelCounter = 0;
+    private float horizontalMove = 0f;
+    private bool jump = false;
 
     private void Start()
     {
-
         enemy = GameObject.FindGameObjectWithTag("Enemy");
     }
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-       
+public class EnemyMovement : MonoBehaviour
+{
+    public static int numEnemy = 0;
+    public CharacterController2D controller;
+    public Animator animator;
+    public GameObject enemy;
+    public float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
 
-            horizontalMove = -1 * runSpeed;
-       
-
-       
-
+    private void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        // Move our character
-        if(runSpeed > 200)
+        horizontalMove = -1 * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (runSpeed > 200)
         {
             jump = true;
-
         }
-     
+
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-     
+
         runSpeed += 0.1f;
-        jump= false;
-    
-
-            // scale the object up by 0.1
-         //   transform.localScale += new Vector3(0.1F, 0.1F, 0);
-     
-        
-
-
+        jump = false;
     }
-    
-    void OnBecameInvisible()
-    {
 
+    private void OnBecameInvisible()
+    {
         if (runSpeed < 300)
         {
             if (runSpeed < 200 && numEnemy < 4)
+            {
                 gameObject.transform.position = new Vector2(3, 0);
+            }
 
             if (runSpeed > 200 && numEnemy < 4)
             {
                 gameObject.transform.position = new Vector2(3, 0);
                 for (int i = 0; i < 1; i++)
                 {
-                    //  Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
                     Instantiate(enemy, new Vector3(i * 2.0F, i * 1.0F, 0), Quaternion.identity);
                 }
                 numEnemy++;
             }
 
-
             gameObject.transform.position = new Vector2(3, 0);
-
         }
-        else {
-            //Instantiate(enemy, new Vector3(3.0F, 1.0F, 0), Quaternion.identity);
+        else
+        {
             gameObject.transform.position = new Vector2(3, 0);
             transform.localScale = new Vector3(6.0F, 6.0F, 0);
-          
-            
         }
-
-        /*
-        if (runSpeed > 300 && numEnemy < 3f)
-        {
-            gameObject.transform.position = new Vector2(3, 0);
-            for (int i = 0; i < 1; i++)
-            {
-                //  Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-                Instantiate(enemy, new Vector3(i * 2.0F, i * 1.0F, 0), Quaternion.identity);
-            }
-            numEnemy++;
-        }
-
-        if (runSpeed > 400 && numEnemy < 4f)
-        {
-            gameObject.transform.position = new Vector2(3, 0);
-            for (int i = 0; i < 1; i++)
-            {
-                //  Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-                Instantiate(enemy, new Vector3(i * 2.0F, i * 1.0F, 0), Quaternion.identity);
-            }
-            numEnemy++;
-        }
-        */
-
     }
-
-
-
-
 }
